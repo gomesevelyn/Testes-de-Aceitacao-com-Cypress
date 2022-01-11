6e473b65e1c9dca7890b4fe463e8fb70f69a15cb
Evelyn Gomes <evelyngomes84@gmail.com>
2022-01-11 10:04:56 -0300

    Cenário de validacao de cpf incorreto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -58,4 +58,61 @@ describe('Cadastro', ()=>{
         cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
 
     })
+
+    it('Usuário com CPF inválido', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        
+        cy.get('a[href="/deliver"]').click() //clica no botão de cadastro
+        
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') //é um checkpoint para verificar se esta no caminho correto
+
+        //massa de dados 
+        var entregador = {
+            nome: 'Evelyn Gomes', 
+            cpf:  '000000141AB',
+            email: 'evelyn@email.com.br',
+            whatsapp: '16999999999',
+            endereco:{
+                cep: '14402460',
+                rua: 'Rua Mário Martins', 
+                numero: '1000',
+                complemento: 'casa',
+                bairro: 'Jardim Paulistano',
+                cidade_uf: 'Franca/SP'
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        //preenchimento dos dados do entregador
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        //preenchimento do endereço
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        //validacao dos campos preenchidos automaticamente pela busca de cep
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        //selecionar tipo de entrega 
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+        //upload arquivo 
+        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+
+        //confirmar preeenchimento do form
+        cy.get('form button[type="submit"]').click()
+
+        //Valida msg de erro 
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+
+    })
 })
